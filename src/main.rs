94216167d49af55f216e0ae8e988d976f5309409
John Kaushagen <johnkaushagen@gmail.com>
2025-09-06 15:49:43 -0400

    add initial mmu

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use std::env;
 use std::fs;
 
+struct Mmu {
+    rom: Vec<u8>,
+}
+
+impl Mmu {
+    pub fn new(rom_data: Vec<u8>) -> Self {
+        Self { rom: rom_data }
+    }
+
+    pub fn read_byte(&self, address: u16) -> u8 {
+        self.rom[address as usize]
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
@@ -12,6 +26,10 @@ fn main() {
     let rom_data = fs::read(rom_path)
         .expect("Faled to read the ROM file.");
 
+    let mmu = Mmu::new(rom_data);
+    let entry_point_instr = mmu.read_byte(0x1000);
+
     println!("Successfully loaded ROM: {}", rom_path);
-    println!("ROM size: {} bytes", rom_data.len());
+    println!("ROM size: {} bytes", mmu.rom.len());
+    println!("First instruction at entry point (0x1000): 0x{:02X}", entry_point_instr);
 }
