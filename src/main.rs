071306aae765af79a3f93c08a3def0d47072f9a0
John Kaushagen <johnkaushagen@gmail.com>
2025-09-06 15:42:08 -0400

    Code to read ROMs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <path_to_rom>", args[0]);
+        return;
+    }
+
+    let rom_path = &args[1];
+    let rom_data = fs::read(rom_path)
+        .expect("Faled to read the ROM file.");
+
+    println!("Successfully loaded ROM: {}", rom_path);
+    println!("ROM size: {} bytes", rom_data.len());
 }
